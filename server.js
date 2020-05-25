8d27ca0ea376a4395359e62ddcb24cf6555c26cb
sali mohamed <sally.elgendy@hotmail.com>
2020-05-25 13:36:50 -0400

    create controllers folder

--- server.js
+++ server.js
@@ -9,204 +9,96 @@ const db = mongoose.connection;
 const methodOverride  = require('method-override');
 const show = console.log
 const tourController =require('./controllers/tour.js')
+const userController = require('./controllers/users_controller.js')
+const session = require('express-session')
+const User = require('./models/users.js')
+const bcrypt = require('bcrypt')
 
 
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/project2';
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true });
+//___________________
+//Middleware
+//___________________
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+require('dotenv').config()
+const port = process.env.PORT||3000
+app.use(
+    sessions({
+      secret: process.env.SECRET, 
+      resave: false, 
+      saveUninitialized: false 
+    })
+  )
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+//controllers 
+app.use('/tour' , tourController)
+
+app.use('/user', userController)
+
+
+
+
+
+
+//mongoose connection
+
+mongoose.connect(process.env.MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 db.on('open' , ()=>{});
 
 
-//___________________
-//Middleware
-//___________________
-
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(methodOverride('_method'));
-app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
-
-//controllers 
-app.use('/tour' , tourController)
-//___________________
-// Routes
-//___________________
-app.get('/' , (req, res) => {
-  res.send('<a href="/tour"> continue </a>');
-});
-
-
-// ROUTE Gallery
-app.get('/tour/gallery', (req, res)=>{
-    res.render('Gallery');
-});
-
-//ROUTE About
-app.get('/tour/about', (req, res)=>{
-    res.render('About');
-});
-
-//ROUTE NEWwwwwww
-app.get('/tour/new', (req, res)=>{
-    res.render('New');
-});
-app.post('/tour/', (req, res)=>{
-
-    // res.send('received');
-    Tour.create(req.body, (error, createdTour)=>{
-        res.redirect('/tour');
-    });
-});
-
-//ROUTE INDEX
-app.get('/tour', (req, res) => {
-    Tour.find({}, (error, allTour) => {
-        res.render('Index', {
-            tour: allTour,
-            // username: req.session.currentUser,
-        });
-    });
-});
-
-// // seed
-// app.get('/tour/seed', (req, res) => {
-//     Tour.create([
-//         {
-//             name: 'Universal Orlando Resort',
-//             location: 'Universal Blvd, Orlando, FL 32819',
-//             img:'https://www.travelingmom.com/wp-content/uploads/2019/04/Universal-Ball-800x450.jpg',
-//             rate: 4.7,
-//             price:233,
-//             size:'35000 sq ft',
-//         },
-//         {
-//             name: 'Walt Disney World Resort',
-//             location: 'Orlando, FL ',
-//             img:'https://img.budgettravel.com/_ampArticle/Disney-World-Seven-Dwarfs.jpg?mtime=20181003120725',
-//             rate:4.7,
-//             price:180,
-//             size:'11sq',
-//         },
-//         {
-//             name: 'Magic kingdom Park',
-//             location: ' Lake Buena Vista, FL 32836',
-//             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQeethLLiBQCEAtdxgr6BtcazWf72CiSHPZvSzeJFy928COrQn&usqp=CAU',
-//             rate:4.7,
-//             price:300,
-//             size:'98900 sq ft',
-//         },
-//         {
-//             name: 'Epcot',
-//             location: '200 Epcot Center Dr, Orlando, FL 32821',
-//             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSnW9vQ9SBNl9hVis4FbUmsb0fu22DpcaV0WO8zZe7-uPpHSBOv&usqp=CAU',
-//             rate:4.7,
-//             price:170,
-//             size:'396400 sq ft',
-//         },
-//            {
-//             name: 'SeaWorld Orlando',
-//             location: ' 7007 Sea World Dr, Orlando, FL 32821',
-//             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTX5Zdy50Rd3dARDWPj7EAjaI5Rt6K-TTf32KUBFy9CUW_dHG4I&usqp=CAU',
-//             rate:4.6,
-//             price:100,
-//             size:'95090 sq ft',
-//         },
-//            {
-//             name: "Universal's Islands Of adventure",
-//             location: 'Universal Blvd, Orlando, FL 32819',
-//             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgoAQ6M-jntbY71xHxBYonx9YAKK4laOgHQE6CRhGA7cMHJZ_w&usqp=CAU',
-//             rate:4.8,
-//             price:350,
-//             size:'35000 sq ft',
-//         },
-//            {
-//             name: "Disney's Hellywood Studio",
-//             location: '351 S Studio Dr, Lake Buena Vista, FL 32830',
-//             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQrSUt6_554sR0C4Zskr30oCXZBA2z_LC3IaMmkonaV4XWDjpb6&usqp=CAU',
-//             rate:4.8,
-//             price:290,
-//             size:'113500 sq ft',
-//         },
-//            {
-//             name: "Disney's Animal Kingdom Theme Park",
-//             location: ' 2901 Osceola Pkwy, Orlando, FL 32830',
-//             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIrdO6xEx1vW0Rw8VE2iNsLT1cr0gZUadK0LmvmzTO-bhOF57l&usqp=CAU',
-//             rate:4.7,
-//             price:450,
-//             size:'35000 sq ft',
-//         },
-        
-       
-//     ], (err, data) => {
-//         res.redirect('/tour');
-//     })
-// });
 
 
 
-//Route show 
-app.get('/tour/:id',(req, res) => {
-    Tour.findById(req.params.id, (error, foundTour) => {
-        res.render('Show', {
-            tour: foundTour,
-        });
-    });
-});
+////////////////
+//Authorization Routes
+////////////////
 
-//DELETE ROUTE
-app.delete('/tour/:id',(req, res) => {
-   Tour.findByIdAndRemove(req.params.id, (err, data) => {
-        res.redirect('/tour');
-    });
-});
-
-
-
-//ROUTE EDIT
-app.get('/tour/edit/:id',(req, res) => {
-    Tour.findById(req.params.id, (error, foundTour) => {
-        res.render('Edit',
-         { tour: foundTour });
-    });
-});
-
-app.put('/tour/edit/:id', (req, res) => {
-    Tour.findByIdAndUpdate(req.params.id, req.body, (error, data) => {
-        res.redirect('/tour');
-    });
-});
+const isAuthenticated = (req, res, next) => {
+    if (req.session.currentUser) {
+        return next();
+    } else {
+        res.redirect('/sessions/new');
+    }
+  };
 
 
 
 
+//LOGIN PAGE
+app.get('/sessions/new', (req, res) => {
+    res.render('sessions/New', { currentUser: req.session.currentUser })
+  })
+app.post('/sessions/', (req, res) => {
+  console.log(req.body)
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    if (err) {
+      res.send('oops the db had a problem')
+    } else if (!foundUser) {
+      res.redirect('/user/new')
+    } else {
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        req.session.currentUser = foundUser
+        res.redirect('/tour/')
+      } else {
+        res.send('wrong password')
+      }
+    }
+  })
+})
 
+app.delete('/sessions/', (req, res) => {
+  req.sessions.destroy(() => {
+    res.redirect('/sessions/new')
+  })
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = session
 
 
 
@@ -214,4 +106,6 @@ app.put('/tour/edit/:id', (req, res) => {
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log( 'Listening port:', PORT));
+app.listen(3000, () => {
+    console.log('listening on: ' + port);
+});
