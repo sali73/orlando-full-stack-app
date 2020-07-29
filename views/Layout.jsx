45ec47e7f28c03c569a6bcbf7509c5d8d578235b
sali mohamed <sally.elgendy@hotmail.com>
2020-07-29 18:42:16 -0400

    change some style

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -9,19 +9,18 @@ const Layout = (props => {
     return (
         <>
             <head>
-            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></link>
-            <link rel="stylesheet" href="/css/style.css"/>
-            <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&family=Raleway:ital,wght@1,500&display=swap" rel="stylesheet"></link>
+              <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous"></link>
+              <link rel="stylesheet" href="/css/style.css"/>
+              <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&family=Raleway:ital,wght@1,500&display=swap" rel="stylesheet"></link>
             </head>
 
             <body className='container-fluid'>
               <div className='body'>
                 <div className='grid'>
                   <nav className="navbar navbar-dark bg-dark">
-
                     <ul>
                       <li><a className="nav" href="/tour"><h1>Home</h1></a></li>
-                      <li><a  className="nav" href="/tour/new"><h3>Create Sightsee </h3></a></li>
+                      <li><a  className="nav" href="/tour/new"><h3>Create Sightseeing </h3></a></li>
                       <li><a className="nav" href="/tour/gallery"><h3>Gallery</h3> </a></li>
                       <li><a className="nav" href="/tour/about"><h3>About</h3></a></li>
                     </ul> 
@@ -34,7 +33,7 @@ const Layout = (props => {
                   <header><img src="/img/2.jpg" alt=""/></header>    
 
                   <main>
-                    <div>{props.children}</div>
+                    <div className='data'>{props.children}</div>
                   </main>
 
                   </div> 
