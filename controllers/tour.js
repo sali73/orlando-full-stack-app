851c0cb5af0bc7a156c18d36bcf0815adb5794b8
sali73 <sally.elgendy@hotmail.com>
2020-11-25 21:52:53 -0500

    uncommit seed and delete session.currentUser from edit and show route

--- controllers/tour.js
+++ controllers/tour.js
@@ -40,77 +40,77 @@ const isAuthenticated = (req, res, next) => {
           });
       });
   });
-  // ROUTE seed to add data to our db
-  // tourController.get('/seed', (req, res) => {
-  //     Tour.create([
-  //         {
-  //             name: 'Universal Orlando Resort',
-  //             location: 'Universal Blvd, Orlando, FL 32819',
-  //             img:'https://www.travelingmom.com/wp-content/uploads/2019/04/Universal-Ball-800x450.jpg',
-  //             rate: 4.7,
-  //             price:233,
-  //             size:'35000 sq ft',
-  //         },
-  //         {
-  //             name: 'Walt Disney World Resort',
-  //             location: 'Orlando, FL ',
-  //             img:'https://img.budgettravel.com/_ampArticle/Disney-World-Seven-Dwarfs.jpg?mtime=20181003120725',
-  //             rate:4.7,
-  //             price:180,
-  //             size:'11sq',
-  //         },
-  //         {
-  //             name: 'Magic kingdom Park',
-  //             location: ' Lake Buena Vista, FL 32836',
-  //             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQeethLLiBQCEAtdxgr6BtcazWf72CiSHPZvSzeJFy928COrQn&usqp=CAU',
-  //             rate:4.7,
-  //             price:300,
-  //             size:'98900 sq ft',
-  //         },
-  //         {
-  //             name: 'Epcot',
-  //             location: '200 Epcot Center Dr, Orlando, FL 32821',
-  //             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSnW9vQ9SBNl9hVis4FbUmsb0fu22DpcaV0WO8zZe7-uPpHSBOv&usqp=CAU',
-  //             rate:4.7,
-  //             price:170,
-  //             size:'396400 sq ft',
-  //         },
-  //            {
-  //             name: 'SeaWorld Orlando',
-  //             location: ' 7007 Sea World Dr, Orlando, FL 32821',
-  //             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTX5Zdy50Rd3dARDWPj7EAjaI5Rt6K-TTf32KUBFy9CUW_dHG4I&usqp=CAU',
-  //             rate:4.6,
-  //             price:100,
-  //             size:'95090 sq ft',
-  //         },
-  //            {
-  //             name: "Universal's Islands Of adventure",
-  //             location: 'Universal Blvd, Orlando, FL 32819',
-  //             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgoAQ6M-jntbY71xHxBYonx9YAKK4laOgHQE6CRhGA7cMHJZ_w&usqp=CAU',
-  //             rate:4.8,
-  //             price:350,
-  //             size:'35000 sq ft',
-  //         },
-  //            {
-  //             name: "Disney's Hellywood Studio",
-  //             location: '351 S Studio Dr, Lake Buena Vista, FL 32830',
-  //             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQrSUt6_554sR0C4Zskr30oCXZBA2z_LC3IaMmkonaV4XWDjpb6&usqp=CAU',
-  //             rate:4.8,
-  //             price:290,
-  //             size:'113500 sq ft',
-  //         },
-  //            {
-  //             name: "Disney's Animal Kingdom Theme Park",
-  //             location: ' 2901 Osceola Pkwy, Orlando, FL 32830',
-  //             img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIrdO6xEx1vW0Rw8VE2iNsLT1cr0gZUadK0LmvmzTO-bhOF57l&usqp=CAU',
-  //             rate:4.7,
-  //             price:450,
-  //             size:'35000 sq ft',
-  //         },    
-  //     ], (err, data) => {
-  //         res.redirect('/tour');
-  //     })
-  // });
+  ROUTE seed to add data to our db
+  tourController.get('/seed', (req, res) => {
+      Tour.create([
+          {
+              name: 'Universal Orlando Resort',
+              location: 'Universal Blvd, Orlando, FL 32819',
+              img:'https://www.travelingmom.com/wp-content/uploads/2019/04/Universal-Ball-800x450.jpg',
+              rate: 4.7,
+              price:233,
+              size:'35000 sq ft',
+          },
+          {
+              name: 'Walt Disney World Resort',
+              location: 'Orlando, FL ',
+              img:'https://img.budgettravel.com/_ampArticle/Disney-World-Seven-Dwarfs.jpg?mtime=20181003120725',
+              rate:4.7,
+              price:180,
+              size:'11sq',
+          },
+          {
+              name: 'Magic kingdom Park',
+              location: ' Lake Buena Vista, FL 32836',
+              img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQeethLLiBQCEAtdxgr6BtcazWf72CiSHPZvSzeJFy928COrQn&usqp=CAU',
+              rate:4.7,
+              price:300,
+              size:'98900 sq ft',
+          },
+          {
+              name: 'Epcot',
+              location: '200 Epcot Center Dr, Orlando, FL 32821',
+              img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSnW9vQ9SBNl9hVis4FbUmsb0fu22DpcaV0WO8zZe7-uPpHSBOv&usqp=CAU',
+              rate:4.7,
+              price:170,
+              size:'396400 sq ft',
+          },
+             {
+              name: 'SeaWorld Orlando',
+              location: ' 7007 Sea World Dr, Orlando, FL 32821',
+              img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTX5Zdy50Rd3dARDWPj7EAjaI5Rt6K-TTf32KUBFy9CUW_dHG4I&usqp=CAU',
+              rate:4.6,
+              price:100,
+              size:'95090 sq ft',
+          },
+             {
+              name: "Universal's Islands Of adventure",
+              location: 'Universal Blvd, Orlando, FL 32819',
+              img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRgoAQ6M-jntbY71xHxBYonx9YAKK4laOgHQE6CRhGA7cMHJZ_w&usqp=CAU',
+              rate:4.8,
+              price:350,
+              size:'35000 sq ft',
+          },
+             {
+              name: "Disney's Hellywood Studio",
+              location: '351 S Studio Dr, Lake Buena Vista, FL 32830',
+              img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQrSUt6_554sR0C4Zskr30oCXZBA2z_LC3IaMmkonaV4XWDjpb6&usqp=CAU',
+              rate:4.8,
+              price:290,
+              size:'113500 sq ft',
+          },
+             {
+              name: "Disney's Animal Kingdom Theme Park",
+              location: ' 2901 Osceola Pkwy, Orlando, FL 32830',
+              img:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIrdO6xEx1vW0Rw8VE2iNsLT1cr0gZUadK0LmvmzTO-bhOF57l&usqp=CAU',
+              rate:4.7,
+              price:450,
+              size:'35000 sq ft',
+          },
+      ], (err, data) => {
+          res.redirect('/tour');
+      })
+  });
   
 
   //Route show 
@@ -119,7 +119,7 @@ const isAuthenticated = (req, res, next) => {
         Tour.findById(req.params.id, (error, foundTour) => {
             res.render('Show', {
                 tour: foundTour,
-                currentUser: req.session.currentUser
+                // currentUser: req.session.currentUser
             });
         })
     }else{
@@ -137,7 +137,6 @@ const isAuthenticated = (req, res, next) => {
       Tour.findById(req.params.id, (error, foundTour) => {
           res.render('Edit',
            { tour: foundTour ,
-            currentUser: req.session.currentUser
         });
       });
   });
