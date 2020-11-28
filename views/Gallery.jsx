a896082e7cc9b0209f7d84fe8ee1c34beb359cd2
sali73 <sally.elgendy@hotmail.com>
2020-11-28 01:10:54 -0500

    change galary photos

--- views/Gallery.jsx
+++ views/Gallery.jsx
@@ -2,48 +2,15 @@ const React = require('react');
 const Layout= require('./Layout.jsx');
 
 class Gallery extends React.Component {
-    render() {
-    return (
-      <Layout>
-          <div className=class="grid">
-              <h1 className='title'> Wellcome To Orlando Gallery</h1>
-                    {/*<div className= 'slider'>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvejO2CInjCNM3lptXr8DMgIpmqO-iE_D5r3qac--pX32WD0Md&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTVLGUpdMEBBeaxJ4YGY6SqdS4qOy_W5XKEfCsuN9wFiz-TgYec&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSP1qduN9tmTMnLH3I2D62tqrlaIIcy1wPHh7yCn2A9atot0Jwn&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_L1hjrzC3ck213Z9U8ejWhl4lX4C_sUBHXN3vda25QteB1mtr&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTb5JvtcvgrpZwkfyD39otkmnHaHsVWuRrXRpUYlsZYr6eiMy8X&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR9_ghoworyfDLoj8bfKMy0D---m6RLVoGeUw4JBBSTgcX94X9D&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSLqLeK7pPO7FbBsC-HSnYLHj_b7w0U23ezK0rn-16NbVwmgbg&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSLNtt16VCcmBEZCLH3RwHlLweqC-0sMzVkGbX4-1QbjpIOigvt&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSJvRVGMmplmtndMEfNmUaaryfCUYWtLTUApzrCyZz_e1FVqmct&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-                    {/*<div>*/}
-                    {/*    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS5kw6a_qUxTQUdcifoq4vJ6cRqBD0d6SM006NnPHu3_8hZ7wEy&usqp=CAU" alt=""/>*/}
-                    {/*</div>*/}
-
-
+       render() {
+        return (
+            <Layout>
+                <div className="jumbotron ">
+                   <h1>Gellary!</h1>
                 </div>
             </Layout>
-        );
+        )
     }
 }
-
 module.exports = Gallery;
 
